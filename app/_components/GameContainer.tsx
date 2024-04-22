9b32d03366a3258a64c98107980e0f04285bd5d3
Braian Pita <braianx100@gmail.com>
2024-04-21 20:53:26 -0500

    layout for game

--- app/_components/GameContainer.tsx
+++ app/_components/GameContainer.tsx
@@ -3,9 +3,16 @@ import PlayingCard from "./PlayingCard";
 
 const GameContainer = () => {
     return (
-        <div className="h-full w-full bg-slate-600 flex flex-col align-middle">
+        <div className="h-[100vh] w-full bg-slate-600 flex flex-col align-middle flex-grow">
             <h1 className="text-center text-5xl text-white my-4">Rock Paper Scissors</h1>
-            <div className="h-100 w-100 grid grid-cols-3 gap-6 p-3 max-w-[900px] self-center">
+
+            <div className="w-100 h-[70%] bg-slate-700 flex flex-col justify-center p-2 gap-2">
+                <div className="bg-slate-800 rounded-md h-2/4 aspect-[5/6] m-auto"></div>
+
+                <div id="allySlot" className="bg-slate-800 rounded-md h-2/4 aspect-[5/6] m-auto"></div>
+            </div>
+
+            <div className="h-[30%] w-100 grid grid-cols-3 gap-6 p-3 max-w-[900px] self-center">
                 <PlayingCard MyIcon={IconMountain} />
                 <PlayingCard MyIcon={IconWallpaper} />
                 <PlayingCard MyIcon={IconScissors} />
--- app/_components/PlayingCard/index.tsx
+++ app/_components/PlayingCard/index.tsx
@@ -3,8 +3,8 @@ import styles from './index.module.scss'
 
 const PlayingCard = ({MyIcon}: {MyIcon: typeof IconMountain}) => {
    return ( 
-   <div className={"flex bg-slate-800 rounded-2xl w-100 h-100 justify-center overflow-clip " +
-                    "align-middle flex-grow aspect-[5/6] hover:rotate-12 hover:transition-transform " + styles.reflection}>
+   <div className={"flex bg-slate-800 rounded-2xl h-full justify-center overflow-clip " +
+                    "align-middle flex-grow aspect-[5/6] hover:scale-110 hover:transition-transform " + styles.reflection}>
         
         <MyIcon className="self-center text-white" size={"75%"} />
 
